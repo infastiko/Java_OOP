c9138c920f1bda20540c805eea6e28b70d848a99
Stanislav Tkachenko <infastiko@gmail.com>
2023-04-18 01:51:04 +0400

    added class Node

---
+++ Homework_1/Node.java
@@ -0,0 +1,17 @@
+package Homework_1;
+
+public class Node {
+    Person p1;
+    Relationships re;
+    Person p2;
+    public Node(Person p1, Relationships re, Person p2) {
+        this.p1 = p1;
+        this.re = re;
+        this.p2 = p2;
+    }
+
+    @Override
+    public String toString() {
+        return String.format( "%s %s %s", p1, re, p2 );
+    }
+}
