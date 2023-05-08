f9bd8418e5753221678c7dcb8ab08fde2a492af9
Stanislav Tkachenko <infastiko@gmail.com>
2023-05-09 02:42:46 +0400

    added class Contact

---
+++ Task_7/Phonebook/Core/Models/Contact.java
@@ -0,0 +1,30 @@
+package Task_7.Phonebook.Core;
+
+public class Contact  implements Comparable<Contact> {
+    public String firstName;
+    public String lastName;
+    public String phone;
+    public String description;
+
+    public Contact(String firstName, String lastName, String description, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+        this.phone = phone;
+    }
+
+
+    @Override
+    public int compareTo(Contact o) {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Contact t = (Contact)obj;
+        return this.firstName == t.firstName
+                && this.lastName == t.lastName
+        ;
+    }
+
+}
