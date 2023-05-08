412b1c60cfb9a1cfd6e55ca9eb7f4da31548f13d
Stanislav Tkachenko <infastiko@gmail.com>
2023-05-09 02:59:49 +0400

    added class Program

---
+++ Task_7/Phonebook/Client/Program.java
@@ -0,0 +1,11 @@
+package Task_7.Phonebook.Client;
+
+import HomeWorkTasks.OOP.Task5.Phonebook.UI.App;
+
+public class Program {
+
+    public static void main(String[] args){
+        
+        App.ButtonClick();
+    }
+}
