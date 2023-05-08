e418bff512cb9dd1bc13648901cabd227a6938e7
Stanislav Tkachenko <infastiko@gmail.com>
2023-05-09 02:25:48 +0400

    added Complex.java

---
+++ Task_7/Calculator/Complex.java
@@ -0,0 +1,50 @@
+package Task7.Calculator;
+
+public class Complex {
+    private final double real;
+    private final double imaginary;
+
+    public Complex
+            (double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public Complex(double real) {
+        this(real, 0);
+    }
+
+    public Complex add(Complex other) {
+        double realPart = this.real + other.real;
+        double imaginaryPart = this.imaginary + other.imaginary;
+        return new Complex(realPart, imaginaryPart);
+    }
+
+    public Complex subtract(Complex other) {
+        double realPart = this.real - other.real;
+        double imaginaryPart = this.imaginary - other.imaginary;
+        return new Complex(realPart, imaginaryPart);
+    }
+
+    public Complex multiply(Complex other) {
+        double realPart = this.real * other.real - this.imaginary * other.imaginary;
+        double imaginaryPart = this.real * other.imaginary + this.imaginary * other.real;
+        return new Complex(realPart, imaginaryPart);
+    }
+
+    public Complex divide(Complex other) {
+        double denominator = other.real * other.real + other.imaginary * other.imaginary;
+        double realPart = (this.real * other.real + this.imaginary * other.imaginary) / denominator;
+        double imaginaryPart = (this.imaginary * other.real - this.real * other.imaginary) / denominator;
+        return new Complex(realPart, imaginaryPart);
+    }
+    @Override
+    public String toString() {
+        if (this.imaginary < 0) {
+            return String.format("%.2f%.2fi", this.real, this.imaginary);
+        } else {
+            return String.format("%.2f+%.2fi", this.real, this.imaginary);
+        }
+    }
+
+}
