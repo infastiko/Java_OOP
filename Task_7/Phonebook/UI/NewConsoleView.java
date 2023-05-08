c4c7e7ade09082c2f6dbe903514c43e41e5680d0
Stanislav Tkachenko <infastiko@gmail.com>
2023-05-09 02:58:45 +0400

    added class NewConsoleView

---
+++ Task_7/Phonebook/UI/NewConsoleView.java
@@ -0,0 +1,15 @@
+package Task_7.Phonebook.UI;
+
+public class NewConsoleView extends ConsoleView {
+    
+    public NewConsoleView() {
+        super();
+    }
+
+    @Override
+    public void setDescription(String value) {
+        super.setDescription(value);
+        System.out.println();
+    }
+
+}
