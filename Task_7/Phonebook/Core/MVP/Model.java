f4221b136497157c169b3e01f6375ce68b86c598
Stanislav Tkachenko <infastiko@gmail.com>
2023-05-09 02:47:00 +0400

    added class Model

---
+++ Task_7/Phonebook/Core/MVP/Model.java
@@ -0,0 +1,76 @@
+package Task_7.Phonebook.Core.MVP;
+
+import Task_7.Phonebook.Core.Infrastructure.Phonebook;
+import Task_7.Phonebook.Core.Models.Contact;
+
+import java.io.*;
+
+public class Model {
+
+    Phonebook currentBook;
+    private int currentIndex;
+    private String path;
+
+    public Model(String path) {
+        currentBook = new Phonebook();
+        currentIndex = 0;
+        this.path = path;
+    }
+
+    public Contact currentContact() {
+        if (currentIndex >= 0) {
+            return currentBook.getCotact(currentIndex);
+        } else {
+            return null;
+        }
+    }
+
+    public void load() {
+        try {
+            File file = new File(path);
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String fname = reader.readLine();
+            while (fname != null) {
+                String lname = reader.readLine();
+                String description = reader.readLine();
+                String phone = reader.readLine();
+                this.currentBook.add(new Contact(fname, lname, description, phone));
+                fname = reader.readLine();
+            }
+            reader.close();
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void save() {
+
+        try (FileWriter writer = new FileWriter(path, false)) {
+            for (int i = 0; i < currentBook.count(); i++) {
+                Contact contact = currentBook.getCotact(i);
+                writer.append(String.format("%s\n", contact.firstName));
+                writer.append(String.format("%s\n", contact.lastName));
+                writer.append(String.format("%s\n", contact.description));
+                writer.append(String.format("%s\n", contact.phone));
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Phonebook currentBook() {
+        return this.currentBook;
+    }
+
+    public int getCurrentIndex() {
+        return this.currentIndex;
+    }
+
+    public void setCurrentIndex(int value) {
+        this.currentIndex = value;
+    }
+}
